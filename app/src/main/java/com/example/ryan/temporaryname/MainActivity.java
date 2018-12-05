0d4fcaece530e4637489672f8c37d3f0d1ca5f8f
unknown <rcouture27@gmail.com>
2018-12-05 02:46:25 -0500

    update

--- app/src/main/java/com/example/ryan/temporaryname/MainActivity.java
+++ app/src/main/java/com/example/ryan/temporaryname/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.ryan.temporaryname;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Toolbar toolbar = findViewById(R.id.toolbar);
+       // setSupportActionBar(toolbar);
     }
-}
+
+    //@Override
+    public boolean onCreatOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return true;
+    }
+
+} // end class
